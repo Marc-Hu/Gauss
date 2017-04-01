1d7a4c8d8910e05d619e75d50ca12de1931dadd0
Marc Hu <marc.hu@outlook.fr>
2017-04-02 00:34:03 +0200

    Intégration d'un menu (pas encore fonctionnel)

---
+++ menu.h
@@ -0,0 +1,32 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include"saisieM.c"
+#include"couleurs_terminal.c"
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+* Structure d'un menu
+* Sa va contenir un tableau qui aura une flèche (quelque part) et pas plus
+* Elle va contenir un tableau de numéro d'un choix par exemple 1 pour jouer, 2 pour sauvegarder etc...
+* Elle contiendra le nombre de choix possible (pour facilité l'initialisation en faite)
+* Et elle contiendra tableau de chaîne de caractère qui contiendra "jouer", "sauvagarder" et ainsi de suite.
+*/
+typedef struct{
+	char *fleche;
+	int *menu;
+	int nbChoix;
+	char **choix;
+} menu;
+
+#endif
+
+void initialisationMenu(menu *p, int nb);
+void libereMemoire(menu *p);
+int flecheValide(menu *p, int a);
+void affichageMenu(menu *p);
+int saisieD();
+int getFleche(menu *p);
+int lanceMenu (menu *p);
+int menuD();
